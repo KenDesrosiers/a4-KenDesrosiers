9083287908366bd99ab875df46f5d8f4521d2e11
Ken Desrosiers <kdesrosiers@wpi.edu>
2019-09-23 12:26:30 -0400

    restarting (didn;t really do anything anyways)

--- public/main.js
+++
@@ -1,5 +0,0 @@
-function initialLoad(){
-  fetch( "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json" )
-    .then( data => data.json())
-    .then( pokemondata => {})
-}
