7fbe7f4ea0fe689580945617fb55842305ff2f5f
Ken Desrosiers <kdesrosiers@wpi.edu>
2019-09-25 10:46:48 -0400

    actually kinda almost done

--- public/main.js
+++ public/main.js
@@ -2,11 +2,22 @@ window.onload = function() {
   fetch( "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json" )
     .then( data => data.json() )
     .then( pokemondata => {
-      d3.select( 'body' ).selectAll( 'div' )
-        .data( d3.entries( pokemondata.pokemon ) )
+          let json = {data: pokemondata.pokemon}
+          let body = JSON.stringify(json);
+          fetch('/', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body
+          })
+          .then(response => response.json())
+          .then(pokemondata => {
+            d3.select( 'body' ).selectAll( 'div.pokemon' )
+        .data( d3.entries(pokemondata.data))
         .join( 'div' )
-          d3.select( 'body' ).selectAll( 'div' )
-          .style('margin-right', '0.5vw')
+          .style('margin-right', '1vw')
+          .attr('class', 'pokemon')
+          .style('margin-bottom', '0.25vw')
+          .style('margin-top', '0.25vw')
           .style('display', 'inline-block')
           .append("img")
           //.attr('src', d => d.value.img)
@@ -14,7 +25,64 @@ window.onload = function() {
           .style('width', '10vw')
           .style('margin', 'auto')
           .style('cursor', 'pointer')
-          .style('border', '2px white solid')
+          .style('border', '2px red solid')
           .style('border-radius', '50%')
+          .on('click', d => fillModal(d))
+          .attr('data-toggle', 'modal')
+          .attr('data-target', '.pokemonentry')
+          })
     })
+}
+
+function fillModal(param){
+  document.querySelector('.modal-title').innerHTML = param.value.name
+  document.querySelector('.pokemontype').innerHTML = param.value.type
+  document.querySelector('.pokemonweaknesses').innerHTML = param.value.weaknesses
+  document.querySelector('.pokemonweight').innerHTML = param.value.weight
+  document.querySelector('.pokemonheight').innerHTML = param.value.height
+  document.querySelector('.pokemonmodalimage').src = param.value.img
+}
+
+function filterAll(){
+  d3.selectAll('.pokemon').remove()
+  //way to get all that are checked
+  let checkedTypes = document.querySelectorAll("#types input:checked")
+  let checkedWeaknesses = document.querySelectorAll("#weaknesses input:checked")
+  let typeArray = [];
+  let weaknessArray = [];
+  checkedTypes.forEach(function(element){
+    typeArray.push(element.value)
+  })
+  checkedWeaknesses.forEach(function(element){
+    weaknessArray.push(element.value)
+  })
+  let json = {types: typeArray, weaknesses: weaknessArray}
+  let body = JSON.stringify(json);
+  fetch('/update', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body
+  })
+  .then(response => response.json())
+  .then(pokemondata => {
+    d3.selectAll( 'div.pokemon' )
+.data( d3.entries(pokemondata.data))
+.join( 'div' )
+  .style('margin-right', '1vw')
+  .attr('class', 'pokemon')
+  .style('margin-bottom', '0.25vw')
+  .style('margin-top', '0.25vw')
+  .style('display', 'inline-block')
+  .append("img")
+  //.attr('src', d => d.value.img)
+  .style('height', '10vw')
+  .style('width', '10vw')
+  .style('margin', 'auto')
+  .style('cursor', 'pointer')
+  .style('border', '2px red solid')
+  .style('border-radius', '50%')
+  .on('click', d => fillModal(d))
+  .attr('data-toggle', 'modal')
+  .attr('data-target', '.pokemonentry')
+  })
 }
--- server.js
+++ server.js
@@ -1,17 +1,60 @@
 let express = require('express'),
 app = express(),
 serveStatic = require('serve-static'),
+bodyParser = require('body-parser'),
 helmet = require('helmet'),
 compression = require('compression'),
 port = process.env.PORT || 3000,
 path = require('path')
 app.use(helmet())
 app.use(compression())
+app.use(bodyParser.json())
+let fulldataset = [],
+activesubset = []
 
 app.use(serveStatic('public', { 'index': false}));
 
 app.get('/', function(req, res) {
     res.sendFile(path.normalize(__dirname + '/public/index.html'))
 });
+app.post('/', function(req, res) {
+    fulldataset = req.body.data
+    activesubset = fulldataset
+    res.send({data: activesubset})
+})
+
+app.post('/update', function(req, res) {
+  activesubset = [];
+  let weaknesses = [],
+  types = []
+  if(req.body.types.length === 0){
+    types = fulldataset
+  } else{
+    fulldataset.forEach(function(element){
+      req.body.types.forEach(function(type){
+        if(element.type.includes(type) && !types.includes(element)){
+          types.push(element)
+        }
+      })
+    })
+  }
+  if(req.body.weaknesses.length === 0){
+    weaknesses = fulldataset
+  } else{
+    fulldataset.forEach(function(element){
+      req.body.weaknesses.forEach(function(weakness){
+        if(element.weaknesses.includes(weakness) && !weaknesses.includes(element)){
+          weaknesses.push(element)
+        }
+      })
+    })
+  }
+  fulldataset.forEach(function(element){
+    if(types.includes(element) && weaknesses.includes(element)){
+      activesubset.push(element)
+    }
+  })
+  res.send({data: activesubset})
+})
 
 app.listen(port);
